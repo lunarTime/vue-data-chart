af12e976231656e41b2103c5969eee1a607ac258
Eremeev Nikita <ena2000.11@gmail.com>
2026-02-11 20:12:25 +0500

    Enhance vite configuration with build options and path aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    base: '/vue-data-chart/',
+    build: {
+        cssCodeSplit: false,
+        rollupOptions: {
+            output: {
+                assetFileNames: 'assets/styles/[name].[ext]'
+            }
+        },
+        minify: 'esbuild',
+        cssMinify: true
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }
 })
